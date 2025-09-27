51fbd7ff409f1a311aed88806f0b7f77eb22d230
Mohiuddin <mohiuddin.niddu@gmail.com>
2025-09-27 21:42:01 +0600

    Add Input component and update dev scripts for web and widget apps

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
+import { add } from "@workspace/math/add";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
@@ -6,7 +8,9 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        <p>2 + 3 = {add(2, 3)}</p>
+        <Input placeholder="Input" />
       </div>
     </div>
-  )
+  );
 }
